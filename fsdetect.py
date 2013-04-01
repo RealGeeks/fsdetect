12c486cb05f64d29a7bab170ee4d3bc74f3f5cfc
Igor Sobreira <igor@igorsobreira.com>
2013-04-01 08:54:11 +0000

    detecting file move, not finished yet

--- fsdetect.py
+++ fsdetect.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 
 import pyinotify
 
-Event = namedtuple('Event', ('pathname',))
+Event = namedtuple('Event', ('pathname', 'src_pathname'))
 
 class Detector(object):
 
@@ -17,10 +17,15 @@ class Detector(object):
             timeout=10
         )
         self._handlers = defaultdict(list)
+        self._previous_moved_from = None
 
     def on(self, event_name, handler):
-        mask = pyinotify.EventsCodes.OP_FLAGS['IN_' + event_name.upper()]
-        maskname = pyinotify.EventsCodes.maskname(mask)
+        if event_name == 'move':
+            mask = pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
+            maskname = 'MOVE'
+        else:
+            mask = pyinotify.EventsCodes.OP_FLAGS['IN_' + event_name.upper()]
+            maskname = pyinotify.EventsCodes.maskname(mask)
 
         self._manager.add_watch(
             path=self._directory,
@@ -37,11 +42,30 @@ class Detector(object):
             self._notifier.process_events()
 
     def _on_event(self, raw_event):
-        maskname = raw_event.maskname
-        if raw_event.mask & pyinotify.IN_ISDIR:
-            maskname = maskname.replace('|IN_ISDIR', '')
+        if raw_event.mask & pyinotify.IN_MOVED_FROM:
+            self._previous_moved_from = raw_event
+            return
 
-        event = Event(raw_event.pathname)
+        if raw_event.mask & pyinotify.IN_MOVED_TO:
+            self._previous_moved_from = None
+            self.notify_handlers(raw_event, 'MOVE')
+        else:
+            if self._previous_moved_from is not None:
+                self.notify_handlers(self._previous_moved_from, 'MOVE')
+                self._previous_moved_from = None
+            self.notify_handlers(raw_event)
+
+    def notify_handlers(self, raw_event, maskname=None):
+        if maskname is None:
+            maskname = raw_event.maskname
+            if raw_event.mask & pyinotify.IN_ISDIR:
+                maskname = maskname.replace('|IN_ISDIR', '')
+
+        event = Event(
+            raw_event.pathname,
+            getattr(raw_event, 'src_pathname', None)
+        )
+
         for handler in self._handlers[maskname]:
             if handler(event):
                 break
--- tests/test_detector.py
+++ tests/test_detector.py
@@ -63,7 +63,7 @@ def test_should_detect_file_creation_recursivelly(tmpdir):
     assert on_create.call_count == 1
 
 
-def test_should_provide_object_with_pathname_attribute_to_handler(tmpdir):
+def test_should_provide_object_with_pathname_attribute_to_handler_on_file_creation(tmpdir):
     basedir = tmpdir.mkdir('mydir')
 
     def on_create(event):
@@ -159,3 +159,138 @@ def test_should_detect_directory_removal(tmpdir):
     detector.check()
 
     assert on_delete.call_count == 1
+
+
+def test_should_provide_object_with_pathname_attribute_to_handler_on_file_removal(tmpdir):
+    basedir = tmpdir.mkdir('mydir')
+    basedir.join('dir1').ensure(dir=True)
+
+    def on_delete(event):
+        assert event.pathname == str(basedir.join('dir1'))
+
+    detector = Detector(str(basedir))
+    detector.on('delete', on_delete)
+
+    os.rmdir(str(basedir.join('dir1')))
+    detector.check()
+
+#
+# 'move' event
+#
+
+def test_should_detect_file_move(tmpdir):
+    basedir = tmpdir.mkdir('mydir')
+    basedir.join('old.txt').ensure(file=True)
+    on_move = mock.Mock(return_value=True)
+
+    detector = Detector(str(basedir))
+    detector.on('move', on_move)
+
+    os.rename(str(basedir.join('old.txt')),
+              str(basedir.join('new.txt')))
+    detector.check()
+
+    assert on_move.call_count == 1
+
+
+def test_should_detect_directory_move(tmpdir):
+    basedir = tmpdir.mkdir('mydir')
+    basedir.join('olddir').ensure(dir=True)
+    on_move = mock.Mock(return_value=True)
+
+    detector = Detector(str(basedir))
+    detector.on('move', on_move)
+
+    os.rename(str(basedir.join('olddir')),
+              str(basedir.join('newdir')))
+    detector.check()
+
+    assert on_move.call_count == 1
+
+
+def test_should_provide_object_with_pathname_and_src_pathname_attributed_to_handler(tmpdir):
+    basedir = tmpdir.mkdir('mydir')
+    basedir.join('old.txt').ensure(file=True)
+
+    def on_move(event):
+        assert event.pathname == str(basedir.join('new.txt'))
+        assert event.src_pathname == str(basedir.join('old.txt'))
+
+    detector = Detector(str(basedir))
+    detector.on('move', on_move)
+
+    os.rename(str(basedir.join('old.txt')),
+              str(basedir.join('new.txt')))
+    detector.check()
+
+
+def test_should_detect_file_move_from_outside_watched_directory(tmpdir):
+    basedir = tmpdir.mkdir('base')
+    basedir.mkdir('watched')
+    basedir.join('old.txt').ensure(file=True)
+    # there is a file on base/old.txt and we are going to watch
+    # base/watched/ diretory. we will detect when old.txt is
+    # moved to base/watched/new.txt
+
+    on_move = mock.Mock(return_value=None)
+
+    detector = Detector(str(basedir.join('watched')))
+    detector.on('move', on_move)
+
+    os.rename(str(basedir.join('old.txt')),
+              str(basedir.join('watched', 'new.txt')))
+    detector.check()
+
+    assert on_move.call_count == 1
+
+
+def test_should_receive_empty_src_pathname_when_file_was_moved_from_outside_watched_directory(tmpdir):
+    basedir = tmpdir.mkdir('base')
+    basedir.mkdir('watched')
+    basedir.join('old.txt').ensure(file=True)
+
+    def on_move(event):
+        assert event.src_pathname is None
+
+    detector = Detector(str(basedir.join('watched')))
+    detector.on('move', on_move)
+
+    os.rename(str(basedir.join('old.txt')),
+              str(basedir.join('watched', 'new.txt')))
+    detector.check()
+
+
+def test_should_detect_file_move_from_inside_watched_directory_to_outside(tmpdir):
+    basedir = tmpdir.mkdir('base')
+    basedir.mkdir('watched').join('old.txt').ensure(file=True)
+    # there is a file file inside 'base/watched', the directory we
+    # are watching. we are going to move this file to outside
+    # the directory we are watching, to 'base/'
+
+    on_move = mock.Mock(return_value=None)
+
+    detector = Detector(str(basedir.join('watched')))
+    detector.on('move', on_move)
+
+    os.rename(str(basedir.join('watched', 'old.txt')),
+              str(basedir.join('new.txt')))
+    # there is a small problem here, if this event (IS_MOVED_FROM)
+    # happened just before an IN_MOVED_TO it means the file is being
+    # moved inside our watched directory, but this case we handle
+    # catching the IN_MOVED_TO event.
+    # what we are simulating here is when the next event has no relation
+    # with the file move.
+    # so unfortunately the detector has to wait until the next event
+    # until he makes a decision.
+    # so let's create a file just to fire another unrelated event
+    detector.check()
+    assert on_move.call_count == 0
+
+    basedir.join('watched', 'useless.txt').ensure(file=True)
+    detector.check()
+
+    assert on_move.call_count == 1
+
+
+def test_should_correctly_detect_two_consecutive_moves_from_inside_watched_directory_to_outside(tmpdir):
+    pass
